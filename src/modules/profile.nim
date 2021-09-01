ed8714cdbaf3db9bcd110b07cb9e67ada76e4e99
Ziad Arafat <ziada@nmsu.edu>
2021-08-31 21:55:02 -0600

    updated profile to fetch more details in a card.

--- src/modules/profile.nim
+++ src/modules/profile.nim
@@ -7,41 +7,59 @@ cmd.addSlash("profile", guildID = &"{gid}") do (server: string, firstname: strin
             kind: irtDeferredChannelMessageWithSource
         )
     )
-
-    var
-        search_url = fmt"https://xivapi.com/character/search?name={firstname.strip()}%20{surname.strip()}&server={server.strip()}&private_key={api_token}"
-        search_content = waitFor client.getContent(search_url)
-        search_parsed_json = search_content.parseJson()
-        results = search_parsed_json["Results"]
-        first_result = results{0}
-
     var embed: Embed
 
-    if first_result.isNil:
-        embed = Embed(
-            title: some("ERROR!"),
-            description: some("Character/Server not found!"),
-        )
-
-    else:
+    try:
         var
-            user_id = first_result["ID"]
-            profile_url = fmt"https://xivapi.com/character/{user_id}?private_key={api_token}"
-            content = waitFor client.getContent(profile_url)
-            parsed_json = content.parseJson()
-            character = parsed_json["Character"]
-            active_class_job = character["ActiveClassJob"]
-            unlocked_state = active_class_job["UnlockedState"]
-            char_name = character["Name"].getStr()
-            portrait_url = character["Portrait"].getStr()
-            level = active_class_job["Level"].getInt()
-            job_name = unlocked_state["Name"].getStr()
-            desc_string = fmt"Level **{level} {job_name}**"
+            searchUrl = fmt"https://ffxiv-character-cards.herokuapp.com/prepare/name/{server.strip()}/{firstname.strip()}%20{surname.strip()}"
+            searchContent = waitFor client.getContent(searchUrl)
+            image = searchContent.parseJson(){"url"}
 
         embed = Embed(
-            title: some(char_name),
-            description: some(desc_string),
-            image: some(EmbedImage(url: some(portrait_url)))
+            image: some(EmbedImage(url: some(fmt"https://ffxiv-character-cards.herokuapp.com{image.getStr()}")))
         )
 
-    await discord.api.editWebhookMessage(s.user.id, i.token, "@original", embeds = @[embed])
+        await discord.api.editWebhookMessage(s.user.id, i.token, "@original", embeds = @[embed])
+        
+
+    except:
+        embed = Embed(
+                title: some("ERROR!"),
+                description: some("Character/Server not found!"),
+            )
+        await discord.api.editWebhookMessage(s.user.id, i.token, "@original", embeds = @[embed])
+
+    # Repurpose this code later
+
+    # var
+    #     searchUrl = fmt"https://xivapi.com/character/search?name={firstname.strip()}%20{surname.strip()}&server={server.strip()}&private_key={apiToken}"
+    #     searchContent = waitFor client.getContent(searchUrl)
+    #     result = searchContent.parseJson()["Results"]{0}
+
+    # var embed: Embed
+
+    # if result.isNil:
+    #     embed = Embed(
+    #         title: some("ERROR!"),
+    #         description: some("Character/Server not found!"),
+    #     )
+
+    # else:
+    #     var
+    #         userId = result["ID"]
+    #         profile_url = fmt"https://xivapi.com/character/{userId}?private_key={apiToken}"
+    #         content = waitFor client.getContent(profile_url)
+    #         character = content.parseJson()["Character"]
+    #         charName: string = character["Name"].getStr()
+    #         portraitUrl: string = character["Portrait"].getStr()
+    #         activeClassJob: JsonNode = character["ActiveClassJob"]
+    #         level = activeClassJob["Level"].getInt()
+    #         jobName = activeClassJob["UnlockedState"]["Name"].getStr()
+    #         descString = fmt"Level **{level} {jobName}**"
+
+    #     embed = Embed(
+    #         title: some(charName),
+    #         description: some(descString),
+    #         image: some(EmbedImage(url: some(portraitUrl)))
+    #     )
+    
